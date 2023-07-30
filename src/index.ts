4e1ed3de73ec3465ed9a26134922e1cd382563b5
Akifumi Niida <nidstyle3@gmail.com>
2023-07-30 11:38:41 +0900

    add: webhook base

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import { Hono } from "hono";
+import { webhook } from "./webhook";
 
-export const port = parseInt(process.env.PORT) || 3000;
+const port = parseInt(process.env.PORT) || 3000;
 
 export const app = new Hono();
 
 app.get('/hello', (c) => c.text('hello'));
+
+export const route = app.route('/webhook', webhook);
+
 
 export default {
   port,
