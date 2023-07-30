c8d15cdd4d01040153939e6cdcaba9fab5caaa0f
Takumi Yamazaki <68004963+takumi-yamazaki-jb@users.noreply.github.com>
2023-07-30 16:30:13 +0900

    Update webhook.ts

--- src/webhook.ts
+++ src/webhook.ts
@@ -8,10 +8,13 @@ webhook.post('notify',async(c) => {
     const headers = {
         'Content-Type': 'application/json'
     }
+    const message = `クーポンが発行されました。
+    下記のサイトでクーポンを取得してください。
+    https://someurl.com/foo?a=${c.req.a}&b=${c.req.b}`;
     const options = {
         method: 'POST',
         headers : headers,
-        body: await c.req.text()
+        body: JSON.stringify({ text: message })
     }
     const res = await fetch(url, options)
     return res;
